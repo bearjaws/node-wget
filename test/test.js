0dba8d0df0b4d81c6211d0ad70ce5c03e9133688
bearjaws <michael.a.barajas@gmail.com>
2017-06-08 21:07:49 -0400

    Added test to not append file

--- test/test.js
+++ test/test.js
@@ -40,4 +40,20 @@ describe("Download Tests", function() {
             expect(progress).to.be.above(0);
         });
     });
+
+    it("Should not append to the previous file.", function(done) {
+        let download = wget.download('http://localhost:8884/file', '/tmp/wget-test-file.bin');
+        download.on('error', function(err) {
+            console.log(err);
+            expect(err).to.be.null;
+            done();
+        });
+        download.on('end', async function(output) {
+            let file = await fs.readFileSync('/tmp/wget-test-file.bin');
+            let hash = crypto.createHash('sha256').update(file).digest('hex');
+            expect(output).to.equal('Finished writing to disk');
+            expect(hash).to.equal(metadata.hash);
+            done();
+        });
+    });
 });
