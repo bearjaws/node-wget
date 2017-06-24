4ec1f5f841a2f4051e399ac3fe6171b898ae32cd
bearjaws <michael.a.barajas@gmail.com>
2017-06-24 02:00:28 -0400

    1) added redirect tests 2) added infinite redirect tests 3) filesize is now a number instead of a string 4) No longer returns NaN if the server does not send a content length 	a) If there is no content length, the "filesize" will be set to 10^length(numberofdownloadedbytes) 	b) this will make progress bars continue to work, but they may jump between 0-99% multiple times 5) Added file size tests

--- test/server.js
+++ test/server.js
@@ -5,27 +5,58 @@ const http = require('http');
 const app = express();
 let size = 1024 * 1024;
 let file = new Buffer(size);
+
+let redirectFile = new Buffer(size);
 crypto.randomBytes(size, function(err, buffer) {
     file = buffer;
 });
 app.get('/file', function (req, res) {
     res.send(file);
 });
-app.get('/file/noSizeHeader', function (req, res) {
-    res.send(file);
-});
+
 app.get('/file/metadata', function (req, res) {
     let hash = crypto.createHash('sha256').update(file).digest('hex');
     res.json({
         'size': size,
         'hash': hash
-    })
+    });
 });
 
-module.exports = async function() {
-    return new Promise((resolve, reject) => {
+app.get('/file/redirect', function(req, res) {
+    let count = 1;
+    if (typeof req.query.count === 'string') {
+        count = Number(req.query.count);
+    }
+    if (count > 3) {
+        res.send(redirectFile);
+    } else {
+        res.redirect('http://localhost:8884/file/redirect?count=' + String(count + 1));
+    }
+});
+
+
+app.get('/file/redirect/infinite', function(req, res) {
+    let count = 1;
+    if (typeof req.query.count === 'string') {
+        count = Number(req.query.count);
+    }
+    res.redirect('http://localhost:8884/file/redirect/infinite?count=' + String(count + 1));
+});
+
+app.get('/file/redirect/metadata', function(req, res) {
+    let hash = crypto.createHash('sha256').update(redirectFile).digest('hex');
+    res.json({
+        'size': size,
+        'hash': hash
+    });
+});
+
+module.exports = function() {
+    return new Promise(function(resolve, reject) {
         app.listen(8884, function () {
-            http.createServer( (request, response) => {
+
+            // This server is used due to the inability to not send content-length headers in express.
+            http.createServer( function(request, response) {
                 response.writeHead(200, {
                     'Content-Type': 'binary',
                 });
--- test/test.js
+++ test/test.js
@@ -7,29 +7,27 @@ let wget = require('../lib/wget');
 
 let baseHTTP = 'http://localhost:8884';
 let metadata = {};
-before(async function() {
+before(function() {
    let server = require('./server');
-   await server();
-   await request(baseHTTP + '/file/metadata', function(err, res, body) {
-       metadata = JSON.parse(body);
+   return server().then(function() {
+       request(baseHTTP + '/file/metadata', function(err, res, body) {
+           metadata = JSON.parse(body);
+       });
    });
 });
 
 describe("Download Tests", function() {
-    // with a proxy:
     it("Should be able to download a file", function(done) {
         let download = wget.download('http://localhost:8884/file', '/tmp/wget-test-file.bin');
         download.on('error', function(err) {
-            console.log(err);
-            expect(err).to.be.null;
-            done();
+            done(err);
         });
         download.on('start', function(fileSize) {
             expect(fileSize).to.be.a('number');
             expect(fileSize).to.equal(metadata.size);
         });
-        download.on('end', async function(output) {
-            let file = await fs.readFileSync('/tmp/wget-test-file.bin');
+        download.on('end', function(output) {
+            let file = fs.readFileSync('/tmp/wget-test-file.bin');
             let hash = crypto.createHash('sha256').update(file).digest('hex');
             expect(output).to.equal('Finished writing to disk');
             expect(hash).to.equal(metadata.hash);
@@ -44,9 +42,7 @@ describe("Download Tests", function() {
     it("Should handle a server that does not have content-length header", function(done) {
         let download = wget.download('http://localhost:9933/', '/tmp/wget-bs-test.bin');
         download.on('error', function(err) {
-            console.log(err);
-            expect(err).to.be.null;
-            done();
+            done(err);
         });
         download.on('start', function(fileSize) {
             expect(fileSize).to.be.null;
@@ -68,11 +64,35 @@ describe("Download Tests", function() {
             expect(err).to.be.null;
             done();
         });
-        download.on('end', async function(output) {
-            let file = await fs.readFileSync('/tmp/wget-test-file.bin');
+        download.on('end', function(output) {
+            let file = fs.readFileSync('/tmp/wget-test-file.bin');
             let hash = crypto.createHash('sha256').update(file).digest('hex');
             expect(output).to.equal('Finished writing to disk');
             expect(hash).to.equal(metadata.hash);
+            done();
+        });
+    });
+
+    it("Should handle 302 redirects that end with file download.", function(done) {
+        let download = wget.download('http://localhost:8884/file/redirect', '/tmp/wget-test-file2.bin');
+        download.on('end', function(output) {
+            request(baseHTTP + '/file/redirect/metadata', function(err, res, body) {
+                let meta = JSON.parse(body);
+                let file = fs.readFileSync('/tmp/wget-test-file2.bin');
+                let hash = crypto.createHash('sha256').update(file).digest('hex');
+                expect(output).to.equal('Finished writing to disk');
+                expect(hash).to.equal(meta.hash);
+                done();
+            });
+
+        });
+    });
+
+    it("Should handle infinite redirects", function(done) {
+        let download = wget.download('http://localhost:8884/file/redirect/infinite', '/tmp/wget-test-file2.bin');
+
+        download.on('error', function(err) {
+            expect(err).to.equal("Infinite redirect loop detected");
             done();
         });
     });
