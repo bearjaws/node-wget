f18bf31f47e37cebc7aee4c9900ffa68fa2e9d3a
Homa Wong <homawong@gmail.com>
2020-01-24 10:53:19 -0800

    feat: add typings to package

---
+++ index.d.ts
@@ -0,0 +1,28 @@
+import { EventEmitter } from 'events'
+import { ClientRequest } from 'http'
+
+declare const wget: {
+  download(src: string, output: string, options?: wget.DownloadOptions): EventEmitter
+  request(options: wget.RequestOptions, callback?: (res: Response) => void): ClientRequest
+}
+
+namespace wget {
+  type RequestOptions = {
+    gunzip?: boolean,
+  } | DownloadOptions
+
+  type DownloadOptions = {
+    proxy?: {
+      protocol?: string,
+      host?: string,
+      port?: number,
+      proxyAuth?: string,
+      headers?: Record<string, string>,
+      method?: string,
+      path?: string,
+      proxy?: string,
+    } | string
+  }
+}
+
+export = wget
